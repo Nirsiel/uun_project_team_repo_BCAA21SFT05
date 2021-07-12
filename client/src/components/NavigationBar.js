f4945c1f818cbd5a87b665772d0739fbc02467e8
Jan Kucera <janci.kucera27112000@gmail.com>
2021-07-12 19:27:35 +0200

    fixed navbar color bug

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 
 const NavigationBar = () => {
   return (
-      <div className="shadow container-fluid">
+      <div className="bg-light shadow container-fluid">
         <Navbar className="p-3 px-5" bg="light">
           <Navbar.Brand href="/">THE COOKBOOK</Navbar.Brand>
           <Nav className="ml-auto">
