3ad897e3fd2c09a3e94c23cca1d8f78571a7b2c7
Jan Kucera <janci.kucera27112000@gmail.com>
2021-07-28 21:58:52 +0200

    Nav and carousel changes
    Search bar moved to Nav bar
    Carousel height set to full view height

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,20 +1,90 @@
-import React from 'react';
-import {Navbar, Nav} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Link} from 'react-router-dom';
+import React, { useCallback, useEffect, useState } from "react";
+import { Button, Form, Container, Row, Carousel } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+import RecipeList from '../components/RecipeOverview/RecipeList';
+import RecipeService from '../services/RecipeService';
 
 const NavigationBar = () => {
-  return (
-      <div className="bg-light shadow container-fluid">
-        <Navbar className="p-3 px-5" bg="light">
-          <Navbar.Brand href="/">THE COOKBOOK</Navbar.Brand>
-          <Nav className="ml-auto">
-            <Link className="link-clearing nav-link" to="/create-recipe">Create
-              recipe</Link>
-          </Nav>
-        </Navbar>
-      </div>
+  const [allRecipes, setAllRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+  const [searchForm, setSearchForm] = useState("");
 
+  const loadRecipesHandler = useCallback(async () => {
+    let data = await RecipeService.getLimitedRecipes(3);
+    setRecipes((prevState) => {
+      return [...prevState, ...data.results];
+    });
+  }, []);
+
+  const loadAllRecipesHandler = useCallback(async () => {
+    let data = await RecipeService.getAllRecipes();
+    setAllRecipes((prevState) => {
+      return [...prevState, ...data.results];
+    });
+  }, []);
+
+
+
+  useEffect(() => {
+    loadAllRecipesHandler();
+    loadRecipesHandler();
+  }, [loadAllRecipesHandler]);
+
+  console.log(allRecipes)
+
+  return (
+    <div className="bg-light shadow container-fluid">
+      <Navbar className="p-3 px-5" bg="light">
+        <Navbar.Brand href="/">THE COOKBOOK</Navbar.Brand>
+        <Nav className="ml-auto">
+          
+
+          <Form className="search-form p-3">
+            <Form.Control
+              className="rounded-0 col-12"
+              type="search"
+              id="search"
+              placeholder="Search"
+              onChange={(event) => {
+                setSearchForm(event.target.value);
+              }}
+            />
+          </Form>
+          <Row className="search">
+            {allRecipes
+              .filter((val) => {
+                if (searchForm !== "") {
+                  return val.name
+                    .toLowerCase()
+                    .includes(searchForm.toLowerCase());
+                  // return val
+                }
+              })
+              .map((val) => {
+                console.log(val)
+                return (
+                  <div className="p-3 search-on">
+                    <Link className="p-2" to={`/show-recipe/${val._id}`}>
+                      <img
+                        className="float-left pt-2"
+                        width="45px"
+                        src={val.picture}
+                      />
+                      <p className="float-left p-2">{val.name}</p>
+                    </Link>
+                  </div>
+                );
+              })}
+          </Row>
+
+          <Link className="link-clearing nav-link my-auto" to="/create-recipe">
+            Create recipe
+          </Link>
+        </Nav>
+      </Navbar>
+    </div>
   );
 };
 
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,17 +1,16 @@
-import React, {useCallback, useEffect, useState} from 'react';
-import {Button, Form, Container, Row, Carousel} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import RecipeList from '../components/RecipeOverview/RecipeList';
-import RecipeService from '../services/RecipeService';
-import MainCarouselItem1 from '../images/carousel-item-1.jpg';
-import MainCarouselItem2 from '../images/carousel-item-2.jpg';
-import MainCarouselItem3 from '../images/carousel-item-3.jpg';
-import {Link} from 'react-router-dom';
+import React, { useCallback, useEffect, useState } from "react";
+import { Button, Form, Container, Row, Carousel } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import RecipeList from "../components/RecipeOverview/RecipeList";
+import RecipeService from "../services/RecipeService";
+import MainCarouselItem1 from "../images/carousel-item-1.jpg";
+import MainCarouselItem2 from "../images/carousel-item-2.jpg";
+import MainCarouselItem3 from "../images/carousel-item-3.jpg";
+import { Link } from "react-router-dom";
 
 const Main = () => {
   const [allRecipes, setAllRecipes] = useState([]);
   const [recipes, setRecipes] = useState([]);
-  const [searchForm, setSearchForm] = useState('');
 
   const loadRecipesHandler = useCallback(async () => {
     let data = await RecipeService.getLimitedRecipes(3);
@@ -40,91 +39,94 @@ const Main = () => {
   }, [loadAllRecipesHandler]);
 
   return (
-      <div className="Main">
-        <Row className="carousel-bug-fix">
-          <Form className="search-form form-inline col-10 mx-auto p-4">
-            <Form.Control className="rounded-0 col-12" type="search" id="search"
-                          placeholder="Search" onChange={(event) => {
-              setSearchForm(event.target.value);
-            }}/>
-            {/* <Button className="rounded-0 search-button" type="submit">
-              Search
-            </Button> */}
+    <div className="Main">
+      <Row className="carousel-bug-fix">
+        <Carousel className="dark-mood">
+          <Carousel.Item className="c-item">
+            <img
+              className="d-block h-100 w-100 c-image"
+              src={MainCarouselItem1}
+              alt="First slide"
+            />
+            <Carousel.Caption className="c-caption">
+              <h1 className="text-light c-header">Delicious</h1>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item className="c-item">
+            <img
+              className="d-block h-100 w-100 c-image"
+              src={MainCarouselItem2}
+              alt="Second slide"
+            />
 
-          </Form>
-          <Row className="search">
-            {allRecipes.filter((val) => {
-              if (searchForm !== '') {
-                return val.name.toLowerCase().includes(searchForm.toLowerCase());
-                // return val
-              }
-            }).map((val) => {
-              return (
-                  <div className="col-10 p-3">
-                    <Link className="p-2" to={`/show-recipe/${val._id}`}>
-                      {/* <Link className="link-clearing text-white" to={`/show-recipe/`}></Link> */}
-                      <img className="float-left" width="40px"
-                           src="https://d1e3z2jco40k3v.cloudfront.net/-/media/mccormick-us/recipes/grill-mates/r/800/roasted-garlic-grilled-vegetables.jpg"/>
-                      <p className="float-left p-2">{val.name}</p>
-                    </Link>
-                  </div>
-              );
-            })}
-          </Row>
-          <Carousel className="dark-mood">
+            <Carousel.Caption className="c-caption">
+              <h1 className="text-light c-header">Amazing</h1>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item className="c-item">
+            <img
+              className="d-block h-100 w-100 c-image"
+              src={MainCarouselItem3}
+              alt="Third slide"
+            />
+
+            <Carousel.Caption className="c-caption">
+              <h1 className="text-light c-header">Flavorful</h1>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+        {/* <Carousel className="dark-mood">
             <Carousel.Item className="c-item">
+              <p>Fuckin delicious</p>
               <img
-                  className="d-block w-100 c-image"
+                  className="d-block h-100 w-100 c-image"
                   src={MainCarouselItem1}
                   alt="First slide"
               />
             </Carousel.Item>
             <Carousel.Item className="c-item">
               <img
-                  className="d-block w-100 c-image"
+                  className="d-block h-100 w-100 c-image"
                   src={MainCarouselItem2}
                   alt="Second slide"
               />
             </Carousel.Item>
             <Carousel.Item className="c-item">
               <img
-                  className="d-block w-100 c-image"
+                  className="d-block h-100 w-100 c-image"
                   src={MainCarouselItem3}
                   alt="Third slide"
               />
             </Carousel.Item>
-          </Carousel>
-        </Row>
+          </Carousel> */}
+      </Row>
 
-
-        <section className="p-4">
-          <Container>
-            <div className="text-center">
-              <h1 className="p-4">Our recipes</h1>
-              <div className="row justify-content-center mb-5">
-                <p className="introduction-text col-8">Browse our full
-                  collection
-                  of inspiring recipe ideas
-                  to cook at home.
-                  It's a virtual world tour of cuisines, from the adventurous to
-                  the familiar.
-                  You'll find quick meals, family favourites and lighter dishes
-                  too.
-                </p>
-              </div>
+      <section className="p-4">
+        <Container>
+          <div className="text-center">
+            <h1 className="p-4">Our recipes</h1>
+            <div className="row justify-content-center mb-5">
+              <p className="introduction-text col-8">
+                Browse our full collection of inspiring recipe ideas to cook at
+                home. It's a virtual world tour of cuisines, from the
+                adventurous to the familiar. You'll find quick meals, family
+                favourites and lighter dishes too.
+              </p>
             </div>
-            {<RecipeList items={recipes}/>}
-            <div className="text-center mt-4 mb-2">
-              <Button onClick={loadNextRecipesHandler}
-                      className="load-button"
-                      type="button">
-                More recipes
-              </Button>
-            </div>
-
-          </Container>
-        </section>
-      </div>
+          </div>
+          {<RecipeList items={recipes} />}
+          <div className="text-center mt-4 mb-2">
+            <Button
+              onClick={loadNextRecipesHandler}
+              className="load-button"
+              type="button"
+            >
+              More recipes
+            </Button>
+          </div>
+        </Container>
+      </section>
+    </div>
   );
 };
 
