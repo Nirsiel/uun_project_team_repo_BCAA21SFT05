469021e667acefe33805c5c090e7223f168e984b
Jan Kucera <janci.kucera27112000@gmail.com>
2021-08-02 20:29:58 +0200

    Carousel photos changes
    New photos compressed and deleted old photos

--- client/src/components/RecipeCreation/NewRecipeForm.js
+++ client/src/components/RecipeCreation/NewRecipeForm.js
@@ -9,64 +9,62 @@ import RatingService from "../../services/RatingService";
 import ReactStars from "react-rating-stars-component";
 import Placeholder from "../../images/placeholder.jpeg";
 
-
 const NewRecipeForm = (props) => {
   const [validated, setValidated] = useState(false);
   const [formState, setFormState] = useState({
-    name: '',
+    name: "",
     timeToPrepare: 0,
-    description: '',
-    instructions: '',
-    ingredients: '',
-    picture: '',
-    keywords: []
+    description: "",
+    instructions: "",
+    ingredients: "",
+    picture: "",
+    keywords: [],
   });
 
   const nameChangeHandler = (event) => {
-    setFormState((prevState => {
-      return {...prevState, name: event.target.value};
-    }));
+    setFormState((prevState) => {
+      return { ...prevState, name: event.target.value };
+    });
   };
   const timeToPrepareChangeHandler = (event) => {
-    setFormState((prevState => {
-      return {...prevState, timeToPrepare: parseInt(event.target.value)};
-    }));
+    setFormState((prevState) => {
+      return { ...prevState, timeToPrepare: parseInt(event.target.value) };
+    });
   };
   const descriptionChangeHandler = (event) => {
-    setFormState((prevState => {
-      return {...prevState, description: event.target.value};
-    }));
+    setFormState((prevState) => {
+      return { ...prevState, description: event.target.value };
+    });
   };
   const instructionsChangeHandler = (event) => {
-    setFormState((prevState => {
-      return {...prevState, instructions: event.target.value};
-    }));
+    setFormState((prevState) => {
+      return { ...prevState, instructions: event.target.value };
+    });
   };
   const ingredientsChangeHandler = (event) => {
-    setFormState((prevState => {
-      return {...prevState, ingredients: event.target.value};
-    }));
+    setFormState((prevState) => {
+      return { ...prevState, ingredients: event.target.value };
+    });
   };
   const pictureChangeHandler = (event) => {
-    setFormState((prevState => {
-      return {...prevState, picture: event.target.value};
-    }));
+    setFormState((prevState) => {
+      return { ...prevState, picture: event.target.value };
+    });
   };
-const keywordChangeHandler = (selected) => {
-  setFormState((prevState => {
-    return {...prevState, keywords: selected};
-  }));
-};
+  const keywordChangeHandler = (selected) => {
+    setFormState((prevState) => {
+      return { ...prevState, keywords: selected };
+    });
+  };
 
-
-const RatingStars = {
-  size: 35,
-  count: 5,
-  isHalf: true,
-  value: 0,
-  color: "grey",
-  activeColor: "grey",
-};
+  const RatingStars = {
+    size: 35,
+    count: 5,
+    isHalf: true,
+    value: 0,
+    color: "grey",
+    activeColor: "grey",
+  };
 
   const formSubmissionHandler = (event) => {
     const form = event.currentTarget;
@@ -75,9 +73,10 @@ const RatingStars = {
       event.preventDefault();
       event.stopPropagation();
     }
-    const materials = formState.ingredients.replace(/\r\n/g, '\n').
-        split('\n').
-        filter(line => line);
+    const materials = formState.ingredients
+      .replace(/\r\n/g, "\n")
+      .split("\n")
+      .filter((line) => line);
 
     const newRecipeData = {
       name: formState.name,
@@ -92,188 +91,178 @@ const RatingStars = {
 
     setValidated(true);
     setFormState({
-      name: '',
+      name: "",
       timeToPrepare: 0,
-      description: '',
-      instructions: '',
-      ingredients: '',
-      picture: '',
+      description: "",
+      instructions: "",
+      ingredients: "",
+      picture: "",
     });
     event.preventDefault();
   };
 
   useEffect(() => {
     if (props.prefetch) {
-      setFormState((prevState => {
+      setFormState((prevState) => {
         return {
           ...prevState,
           name: props.prefetch.name,
           timeToPrepare: props.prefetch.timeToPrepare,
           description: props.prefetch.description,
           instructions: props.prefetch.instructions,
-          ingredients: props.prefetch.materials.join('\n'),
+          ingredients: props.prefetch.materials.join("\n"),
           picture: props.prefetch.picture,
         };
-      }));
+      });
       //TODO: set selected keywords to the dropdown menu
     }
   }, [props.keywords, props.prefetch]);
 
   const pageName = () => {
     let urlPathname = window.location.pathname;
-    urlPathname = urlPathname.split("/")
-    let header
+    urlPathname = urlPathname.split("/");
+    let header;
 
-    if(urlPathname[1] === "update-recipe") {
-      header = "Updade recipe"
+    if (urlPathname[1] === "update-recipe") {
+      header = "Updade recipe";
     } else {
-      header = "Create Recipe"
+      header = "Create Recipe";
     }
 
-    return header
-  }
+    return header;
+  };
 
-  if (props.items.length === 0){
-    return (
-        <div>No keywords loaded...</div>
-    );
+  if (props.items.length === 0) {
+    return <div>No keywords loaded...</div>;
   }
 
   console.log("form state: ");
   console.log(formState);
   return (
     <section className="pt-7 bg-light">
-       <h1 className="text-center pb-2">{pageName()}</h1>
-    <Form className="add-form shadow p-0 pt-4 pb-4"noValidate
-      validated={validated}
-     onSubmit={formSubmissionHandler}>
-      <Container>
-        <Row className="pb-5">
-          <Col>
-            <Image className="recipe-profile-photo" src={Placeholder} fluid />
-            <span className="cooking-time badge">
-              <i className="inline far fa-clock" />
-              <Form.Group controlId="create-recipe-form2">
-                <Form.Control
-                  placeholder="10min"
-                  value={formState.timeToPrepare}
-                  onChange={timeToPrepareChangeHandler}
-                  className="badge-time-to-prepare"
-                  required
-                  min="0"
-                  max="2880"
-                  type="number"
-                />
-              </Form.Group>
-            </span>
-            <Form.Group controlId="create-recipe-form4">
-              <Form.Control
-                value={formState.picture}
-                onChange={pictureChangeHandler}
-                placeholder="Place your Picture URL"
-                required
-                type="text"
-              />
-            </Form.Group>
-          </Col>
-          <Col className="mx-3">
-            <Form.Group controlId="create-recipe-form1">
-              <Form.Control
-                className="testujeme"
-                value={formState.name}
-                onChange={nameChangeHandler}
-                placeholder="Name your Recipe"
-                required
-                type="text"
-              />
-            </Form.Group>
-            <div>
-              <ReactStars {...RatingStars} />
-              <p className="faded-text">(You can't add ratings yet)</p>
-            </div>
-            <Form.Group className="pt-4" controlId="create-recipe-form4">
-              <Form.Control
-                placeholder="Describe how awesome your recipe is"
-                required
-                value={formState.description}
-                onChange={descriptionChangeHandler}
-                as="textarea"
-                rows={3}
-              />
-            </Form.Group>
-            <br />
-            <Form.Group controlId="create-recipe-form4">
-              <Form.Label className="pl-1">Key Words</Form.Label>
-              <DropdownMultiselect
-                className="add-form-dropdown"
-                handleOnChange={keywordChangeHandler}
-                options={props.items.map((keyword) => {
-                  return {key: keyword._id, label: keyword.value}
-                })}
-                name="countries"
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-      </Container>
-      <article className="pt-3">
-        <Container className="p-4 pb-5">
-          <Row className="align-items-start">
-            <Col className="ingredient-list bg-white" xs={4}>
-              <ul className="p-3">
-                <li>
-                  <h3>Ingredients</h3>
-                </li>
-              </ul>
+      <h1 className="text-center pb-2">{pageName()}</h1>
+      <Form
+        className="add-form shadow p-0 pt-4 pb-4"
+        noValidate
+        validated={validated}
+        onSubmit={formSubmissionHandler}
+      >
+        <Container>
+          <Row className="pb-5">
+            <Col>
+              <Image className="recipe-profile-photo" src={Placeholder} fluid />
+              <span className="cooking-time badge">
+                <i className="inline far fa-clock" />
+                <Form.Group controlId="create-recipe-form2">
+                  <Form.Control
+                    placeholder="10min"
+                    value={formState.timeToPrepare}
+                    onChange={timeToPrepareChangeHandler}
+                    className="badge-time-to-prepare"
+                    required
+                    min="0"
+                    max="2880"
+                    type="number"
+                  />
+                </Form.Group>
+              </span>
               <Form.Group controlId="create-recipe-form4">
                 <Form.Control
-                  placeholder='Split ingredient name and amount by using colon ":"'
+                  value={formState.picture}
+                  onChange={pictureChangeHandler}
+                  placeholder="Place your Picture URL"
                   required
-                  value={formState.ingredients}
-                  onChange={ingredientsChangeHandler}
                   type="text"
-                  as="textarea"
                 />
               </Form.Group>
             </Col>
-            <Col className="bg-white" md={{ span: 7, offset: 1 }}>
-              <ul className="p-3">
-                <li className="">
-                  <h3>Cooking instructions</h3>
-                </li>
-              </ul>
-              <Form.Group controlId="create-recipe-form4">
+            <Col className="mx-3">
+              <Form.Group controlId="create-recipe-form1">
                 <Form.Control
-                  value={formState.instructions}
-                  onChange={instructionsChangeHandler}
-                  placeholder="Split steps by using Enter"
+                  className="testujeme"
+                  value={formState.name}
+                  onChange={nameChangeHandler}
+                  placeholder="Name your Recipe"
                   required
+                  type="text"
+                />
+              </Form.Group>
+              <div>
+                <ReactStars {...RatingStars} />
+                <p className="faded-text">(You can't add ratings yet)</p>
+              </div>
+              <Form.Group className="pt-4" controlId="create-recipe-form4">
+                <Form.Control
+                  placeholder="Describe how awesome your recipe is"
+                  required
+                  value={formState.description}
+                  onChange={descriptionChangeHandler}
                   as="textarea"
                   rows={3}
+                />
+              </Form.Group>
+              <br />
+              <Form.Group controlId="create-recipe-form4">
+                <Form.Label className="pl-1">Key Words</Form.Label>
+                <DropdownMultiselect
+                  className="add-form-dropdown"
+                  handleOnChange={keywordChangeHandler}
+                  options={props.items.map((keyword) => {
+                    return { key: keyword._id, label: keyword.value };
+                  })}
+                  name="countries"
                 />
               </Form.Group>
             </Col>
           </Row>
         </Container>
-      </article>
-      <Row>
-        <Button
-          className="mt-4 mr-3 offset-5  col-2"
-          variant="secondary"
-          type="submit"
-        >
-          <i class="fas fa-trash"></i> Dump this recipe
-        </Button>
-        <Button
-          className="mt-4 color col-3"
-          type="submit"
-        >
-          Submit
-        </Button>
-      </Row>
-      
-    </Form>
-  </section>
+        <article className="pt-3">
+          <Container className="p-4 pb-5">
+            <Row className="align-items-start">
+              <Col className="ingredient-list bg-white" xs={4}>
+                <ul className="p-3">
+                  <li>
+                    <h3>Ingredients</h3>
+                  </li>
+                </ul>
+                <Form.Group controlId="create-recipe-form4">
+                  <Form.Control
+                    placeholder='Split ingredient name and amount by using colon ":"'
+                    required
+                    value={formState.ingredients}
+                    onChange={ingredientsChangeHandler}
+                    type="text"
+                    as="textarea"
+                  />
+                </Form.Group>
+              </Col>
+              <Col className="bg-white" md={{ span: 7, offset: 1 }}>
+                <ul className="p-3">
+                  <li className="">
+                    <h3>Cooking instructions</h3>
+                  </li>
+                </ul>
+                <Form.Group controlId="create-recipe-form4">
+                  <Form.Control
+                    value={formState.instructions}
+                    onChange={instructionsChangeHandler}
+                    placeholder="Split steps by using Enter"
+                    required
+                    as="textarea"
+                    rows={3}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+          </Container>
+        </article>
+        <Row>
+          <Button className="mt-4 color offset-6 col-3" type="submit">
+            Save recipe
+          </Button>
+        </Row>
+      </Form>
+    </section>
     // <Form
     //   className="add-form shadow p-0 pt-4 pb-4"
     //   noValidate
